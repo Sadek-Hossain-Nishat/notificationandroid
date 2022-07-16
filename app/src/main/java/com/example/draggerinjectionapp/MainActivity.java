da52a172f6749a5eaccb692d6e63693614ddd950
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-07-16 09:13:32 -0700

    commit-multiple-notifications-insummary-group

--- app/src/main/java/com/example/draggerinjectionapp/MainActivity.java
+++ app/src/main/java/com/example/draggerinjectionapp/MainActivity.java
@@ -31,41 +31,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText titleET, messageET;
     private NotificationManagerCompat notificationManagerCompat;
 
-    static List<Message> MESSAGES = new ArrayList<>() ;
-
-
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        notificationManagerCompat =NotificationManagerCompat.from(this);
-        titleET = findViewById(R.id.id_edit_text_title);
-        messageET = findViewById(R.id.id_edit_text_message);
-
-
-        Person jim = new Person.Builder().setName("Jim").build();
-        Person jenny = new Person.Builder().setName("Jenny").build();
-
-        MESSAGES.add(new Message("Good morning",jim));
-        MESSAGES.add(new Message("Hello",null));
-        MESSAGES.add(new Message("Hi",jenny));
-
-
-    }
-
-    public  void sendOnChannel1(View view){
-        sendChannel1Notification(this);
-
-    }
-
+    static List<Message> MESSAGES = new ArrayList<>();
 
     public static void sendChannel1Notification(Context context) {
 
 
-        Intent activityIntent =new Intent(context,MainActivity.class);
+        Intent activityIntent = new Intent(context, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context,
-                0,activityIntent,0);
+                0, activityIntent, 0);
 
 
         RemoteInput remoteInput = new RemoteInput.Builder("key_text_reply")
@@ -73,15 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
 
-        Intent replyIntent ;
-        PendingIntent replyPendingIntent =null;
+        Intent replyIntent;
+        PendingIntent replyPendingIntent = null;
 
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.N){
-            replyIntent  = new Intent(context,DirectReplyReceiver.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            replyIntent = new Intent(context, DirectReplyReceiver.class);
             replyPendingIntent = PendingIntent.getBroadcast(context,
-                    0,replyIntent,0);
+                    0, replyIntent, 0);
 
-        }else {
+        } else {
             // start chat activity instead (PendingIntent.getActivity)
             //cancel notification with notificationManagerCompat.Cancel(id)
         }
@@ -96,14 +69,12 @@ public class MainActivity extends AppCompatActivity {
         Person sender = new Person.Builder().setName("Me").build();
 
 
-
-
         NotificationCompat.MessagingStyle messagingStyle =
                 new NotificationCompat.MessagingStyle(sender);
         messagingStyle.setConversationTitle("Group Chat");
 
 
-        for (Message chatMessage:MESSAGES) {
+        for (Message chatMessage : MESSAGES) {
 
 
             NotificationCompat.MessagingStyle.Message notificationMessage =
@@ -118,9 +89,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
-
-        Notification notification =new NotificationCompat.Builder(context,
+        Notification notification = new NotificationCompat.Builder(context,
                 CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
 
@@ -139,51 +108,93 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
 
-        NotificationManagerCompat notificationManagerCompat =NotificationManagerCompat.from(context);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
 
-        notificationManagerCompat.notify(1,notification);
+        notificationManagerCompat.notify(1, notification);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        titleET = findViewById(R.id.id_edit_text_title);
+        messageET = findViewById(R.id.id_edit_text_message);
+
+
+        Person jim = new Person.Builder().setName("Jim").build();
+        Person jenny = new Person.Builder().setName("Jenny").build();
+
+        MESSAGES.add(new Message("Good morning", jim));
+        MESSAGES.add(new Message("Hello", null));
+        MESSAGES.add(new Message("Hi", jenny));
+
+
+    }
+
+    public void sendOnChannel1(View view) {
+        sendChannel1Notification(this);
+
     }
 
     public void sendOnChannel2(View view) {
-        final int progressMax = 100;
+
+        String title1 = "Title 1";
+        String message1 = "Message 1";
+
+        String title2 = "Title 2";
+        String message2 = "Message 2";
 
 
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this,
+        Notification notification1 = new NotificationCompat.Builder(this,
                 CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
-                .setContentTitle("Download")
-                .setContentText("Download in progress")
+                .setContentTitle(title1)
+                .setContentText(message1)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setOngoing(true)
-                .setOnlyAlertOnce(true)
-                // false for determinate progress
-                .setProgress(progressMax, 0, false);
-        // inderterminate progress
-        // it will be true
-        // and setprogress will be commented out
+                .setGroup("example_group")
+                .build();
 
-        notificationManagerCompat.notify(2, notification.build());
+        Notification notification2 = new NotificationCompat.Builder(this,
+                CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_two)
+                .setContentTitle(title2)
+                .setContentText(message2)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setGroup("example_group")
+                .build();
 
-        new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        SystemClock.sleep(2000);
-                        for (int progress = 0; progress <= progressMax; progress += 10) {
 
-                            notification.setProgress(progressMax, progress, false);
-                            notificationManagerCompat.notify(2, notification.build());
-                            SystemClock.sleep(1000);
+        Notification summarynotification = new NotificationCompat.Builder(this,
+                CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_reply)
+                .setStyle(
 
-                        }
-                        notification.setContentText("Download finished")
-                                .setProgress(0, 0, false)
-                                .setOngoing(false);
-                        notificationManagerCompat.notify(2, notification.build());
+                        new NotificationCompat.InboxStyle()
+                                .addLine(
+                                        title2 + "  " + message2
+                                )
 
-                    }
-                }
-        ).start();
+                                .addLine(
+                                        title1 + "  " + message1
+                                )
+                                .setBigContentTitle("2 new messages")
+                                .setSummaryText("user@example.com")
+
+                )
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setGroup("example_group")
+                .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
+                .setGroupSummary(true)
+                .build();
+
+        SystemClock.sleep(2000);
+        notificationManagerCompat.notify(2, notification1);
+        SystemClock.sleep(2000);
+        notificationManagerCompat.notify(3, notification2);
+        SystemClock.sleep(2000);
+        notificationManagerCompat.notify(4, summarynotification);
+
 
     }
 }
